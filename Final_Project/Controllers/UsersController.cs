7e3475f6a8ad3bdac79e4fe57d12971aedb1dd6a
Rajarshee Roy <bitturoy32001@gmail.com>
2024-09-01 11:56:02 +0530

    Buttons in Row - Roy

--- Final_Project/Controllers/UsersController.cs
+++ Final_Project/Controllers/UsersController.cs
@@ -20,180 +20,245 @@ namespace Final_Project.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var users = await _userManager.Users.Select(user => new UserViewModel
+            try
             {
-                Id = user.Id,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Email = user.Email,
-                Roles = _userManager.GetRolesAsync(user).Result
-            }).ToListAsync();
-            return View(users);
+                var users = await _userManager.Users.Select(user => new UserViewModel
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Roles = _userManager.GetRolesAsync(user).Result
+                }).ToListAsync();
+                return View(users);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public async Task<IActionResult> Add()
         {
-            var roles = await _roleManager.Roles.Select(r => new RoleViewModel { RoleId = r.Id, RoleName = r.Name }).ToListAsync();
-
-            var viewModel = new AddUserViewModel
+            try
             {
-                Roles = roles
-            };
-            return View(viewModel);
+                var roles = await _roleManager.Roles.Select(r => new RoleViewModel { RoleId = r.Id, RoleName = r.Name }).ToListAsync();
+
+                var viewModel = new AddUserViewModel
+                {
+                    Roles = roles
+                };
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Add(AddUserViewModel model)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return View(model);
-            }
-
-            if (!model.Roles.Any(r => r.IsSelected))
-            {
-                ModelState.AddModelError("Roles", "Please select at least one role");
-                return View(model);
-            }
-
-
-            var user = new ApplicationUser
-            {
-                UserName = model.UserName,
-                Email = model.Email,
-                FirstName = model.FirstName,
-                LastName = model.LastName
-            };
-
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (!result.Succeeded)
-            {
-                foreach (var error in result.Errors)
+                if (!ModelState.IsValid)
                 {
-                    ModelState.AddModelError("Roles", error.Description);
+                    return View(model);
                 }
 
-                return View(model);
-            }
-            await _userManager.AddToRolesAsync(user, model.Roles.Where(r => r.IsSelected).Select(r => r.RoleName));
+                if (!model.Roles.Any(r => r.IsSelected))
+                {
+                    ModelState.AddModelError("Roles", "Please select at least one role");
+                    return View(model);
+                }
 
+
+                var user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("Roles", error.Description);
+                    }
+
+                    return View(model);
+                }
+                await _userManager.AddToRolesAsync(user, model.Roles.Where(r => r.IsSelected).Select(r => r.RoleName));
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return RedirectToAction(nameof(Index));
         }
 
         public async Task<IActionResult> Edit(string userId)
         {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
+            try
             {
-                return NotFound();
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                var viewModel = new ProfileFormViewModel
+                {
+                    Id = userId,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    UserName = user.UserName,
+                    Email = user.Email
+                };
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
 
-            var viewModel = new ProfileFormViewModel
-            {
-                Id = userId,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                UserName = user.UserName,
-                Email = user.Email
-            };
-            return View(viewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(ProfileFormViewModel model)
         {
-            if (!ModelState.IsValid)
-                return View(model);
-            var user = await _userManager.FindByIdAsync(model.Id);
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(model);
+                var user = await _userManager.FindByIdAsync(model.Id);
 
-            if (user == null)
-                return NotFound();
-            var userWithSameEmail = await _userManager.FindByEmailAsync(model.Email);
-            if (userWithSameEmail != null && userWithSameEmail.Id != model.Id)
-            {
-                ModelState.AddModelError("Email", "This email is already assigned to another user");
-                return View(model);
+                if (user == null)
+                    return NotFound();
+                var userWithSameEmail = await _userManager.FindByEmailAsync(model.Email);
+                if (userWithSameEmail != null && userWithSameEmail.Id != model.Id)
+                {
+                    ModelState.AddModelError("Email", "This email is already assigned to another user");
+                    return View(model);
+                }
+                var userWithSameUserName = await _userManager.FindByEmailAsync(model.Email);
+                if (userWithSameUserName != null && userWithSameUserName.Id != model.Id)
+                {
+                    ModelState.AddModelError("UserName", "This username is already assigned to another user");
+                    return View(model);
+                }
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                await _userManager.UpdateAsync(user);
             }
-            var userWithSameUserName = await _userManager.FindByEmailAsync(model.Email);
-            if (userWithSameUserName != null && userWithSameUserName.Id != model.Id)
+            catch (Exception)
             {
-                ModelState.AddModelError("UserName", "This username is already assigned to another user");
-                return View(model);
+
+                throw;
             }
-            user.FirstName = model.FirstName;
-            user.LastName = model.LastName;
-            user.UserName = model.UserName;
-            user.Email = model.Email;
-            await _userManager.UpdateAsync(user);
             return RedirectToAction(nameof(Index));
         }
 
         public async Task<IActionResult> ManageRoles(string userId)
         {
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
+            try
             {
-                return NotFound();
-            }
-
-            var roles = await _roleManager.Roles.ToListAsync();
-
-            var viewModel = new UserRolesViewModel
-            {
-                UserId = user.Id,
-                UserName = user.UserName,
-                Roles = roles.Select(role => new RoleViewModel
+                var user = await _userManager.FindByIdAsync(userId);
+                if (user == null)
                 {
-                    RoleId = role.Id,
-                    RoleName = role.Name,
-                    IsSelected = _userManager.IsInRoleAsync(user, role.Name).Result
-                }).ToList()
-            };
-            return View(viewModel);
+                    return NotFound();
+                }
+
+                var roles = await _roleManager.Roles.ToListAsync();
+
+                var viewModel = new UserRolesViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    Roles = roles.Select(role => new RoleViewModel
+                    {
+                        RoleId = role.Id,
+                        RoleName = role.Name,
+                        IsSelected = _userManager.IsInRoleAsync(user, role.Name).Result
+                    }).ToList()
+                };
+                return View(viewModel);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ManageRoles(UserRolesViewModel model)
         {
-            var user = await _userManager.FindByIdAsync(model.UserId);
-
-            if (user == null)
-                return NotFound();
-
-            var userRoles = await _userManager.GetRolesAsync(user);
-
-            foreach (var role in model.Roles)
+            try
             {
-                if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
-                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+                var user = await _userManager.FindByIdAsync(model.UserId);
 
-                if (!userRoles.Any(r => r == role.RoleName) && role.IsSelected)
-                    await _userManager.AddToRoleAsync(user, role.RoleName);
+                if (user == null)
+                    return NotFound();
+
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                foreach (var role in model.Roles)
+                {
+                    if (userRoles.Any(r => r == role.RoleName) && !role.IsSelected)
+                        await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+
+                    if (!userRoles.Any(r => r == role.RoleName) && role.IsSelected)
+                        await _userManager.AddToRoleAsync(user, role.RoleName);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
 
             return RedirectToAction(nameof(Index));
         }
-        
+
 
 
         [HttpPost]
         public async Task<IActionResult> Delete(string userId)
         {
-            var user = await _userManager.FindByIdAsync(userId);
+            try
+            {
+                var user = await _userManager.FindByIdAsync(userId);
 
-            // Check if the user exists
-            if (user == null)
-                return NotFound();
+                // Check if the user exists
+                if (user == null)
+                    return NotFound();
 
-            // Attempt to delete the user
-            var result = await _userManager.DeleteAsync(user);
+                // Attempt to delete the user
+                var result = await _userManager.DeleteAsync(user);
 
-            // Check if the deletion was successful
-            if (!result.Succeeded)
-                throw new Exception("Error deleting user");
+                // Check if the deletion was successful
+                if (!result.Succeeded)
+                    throw new Exception("Error deleting user");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
             // Redirect to the index action (or whichever action lists the users)
             return RedirectToAction("Index");
---
+++ Final_Project/Data/Migrations/20240901022724_Fetching entire UI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Final_Project.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FetchingentireUI : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Appointment",
+                keyColumn: "AppId",
+                keyValue: 1,
+                columns: new[] { "EndDateTime", "StartDateTime" },
+                values: new object[] { new DateTime(2024, 9, 3, 7, 57, 23, 276, DateTimeKind.Local).AddTicks(3586), new DateTime(2024, 9, 1, 7, 57, 23, 276, DateTimeKind.Local).AddTicks(3567) });
+
+            migrationBuilder.UpdateData(
+                table: "Appointment",
+                keyColumn: "AppId",
+                keyValue: 2,
+                columns: new[] { "EndDateTime", "StartDateTime" },
+                values: new object[] { new DateTime(2024, 9, 4, 7, 57, 23, 276, DateTimeKind.Local).AddTicks(3596), new DateTime(2024, 9, 1, 7, 57, 23, 276, DateTimeKind.Local).AddTicks(3595) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Appointment",
+                keyColumn: "AppId",
+                keyValue: 1,
+                columns: new[] { "EndDateTime", "StartDateTime" },
+                values: new object[] { new DateTime(2024, 9, 2, 10, 53, 40, 964, DateTimeKind.Local).AddTicks(2991), new DateTime(2024, 8, 31, 10, 53, 40, 964, DateTimeKind.Local).AddTicks(2974) });
+
+            migrationBuilder.UpdateData(
+                table: "Appointment",
+                keyColumn: "AppId",
+                keyValue: 2,
+                columns: new[] { "EndDateTime", "StartDateTime" },
+                values: new object[] { new DateTime(2024, 9, 3, 10, 53, 40, 964, DateTimeKind.Local).AddTicks(3000), new DateTime(2024, 8, 31, 10, 53, 40, 964, DateTimeKind.Local).AddTicks(2999) });
+        }
+    }
+}
